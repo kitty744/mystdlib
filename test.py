cf4b8f26264a9eb885ddf78edbbc9c46009b7bad
Trollycat <kittygamingbuisness@gmail.com>
2025-12-06 08:38:39 -0500

    Test code example

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-from mylib import printer
+from mylib import *
+
+printer.println("Hello, World!")
+printer.println("Here is the current date and time: " + time.now())
+printer.println("Here is a random number: " + random.random())
